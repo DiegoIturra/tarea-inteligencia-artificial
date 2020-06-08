e24ba59acc8156bbef50c84ce329430af5a37bb7
Diego Iturra <33190592+DiegoIturra@users.noreply.github.com>
2020-06-07 20:34:36 -0400

    Add files via upload

--- tarea.cpp
+++ tarea.cpp
@@ -11,7 +11,6 @@
 #include <utility>
 #include <numeric>
 using namespace std;
-
 typedef vector<vector<int>> Puzzle;
 
 //denota la posicion de un elemento en el puzzle
@@ -20,18 +19,29 @@ struct Posicion{
     int y;
 };
 
+struct Matriz{
+    Puzzle estado;
+    Puzzle parent;
+    int f;
+    int h;
+    int g;
+
+    //para poder insertar dentro de un set ordenado con menor f
+    bool operator<(const Matriz& m) const{
+      return (this->f < m.f);
+    }
+};
+
+
 //comparador
 struct myComp{
-    constexpr bool operator()(
-        pair<Puzzle,int> const& a,
-        pair<Puzzle,int> const& b)
-        const noexcept{
+    constexpr bool operator()(pair<Puzzle,int> const& a, pair<Puzzle,int> const& b) const noexcept{
             return a.second > b.second;
-        }
+    }
 };
 
 //funcion que imprime un tablero
-void imprimirTablero(Puzzle& matriz,int& size){
+void imprimirTablero(Puzzle& matriz,int size){
     for(int i=0 ; i<size ; i++){
         for(int j=0 ; j<size ; j++){
             cout << matriz[i][j] << " ";
@@ -91,7 +101,7 @@ Puzzle moveLeft(Puzzle& estadoActual , int& size ,pair<int,int>& posCero){
 }
 
 //esta funcion comprueba si el estado actual es igual al estado al cual queremos llegar
-bool testObjetivo(Puzzle& goal ,Puzzle& current , const int& size){
+bool testObjetivo(Puzzle goal ,Puzzle& current , const int& size){
     for(int i=0 ; i<size ; i++){
         for(int j=0 ; j<size ; j++){
             if(goal[i][j] != current[i][j]){
@@ -120,12 +130,12 @@ Puzzle getObjetivo(const int& size){
 }
 
 //este metodo retorna true si el top del stack es igual al nodo actual,en otro caso retorna false
-bool enFrontera(stack<Puzzle>& frontera,Puzzle& estadoActual){
+bool enFrontera(queue<Puzzle>& frontera,Puzzle& estadoActual){
     Puzzle topFrontera;
     bool onTop = false;
     //si el stack no está vacio
     if(!frontera.empty()){
-        topFrontera = frontera.top();
+        topFrontera = frontera.front();
         onTop = true;
     }
     //si hay un elemento en el top,comprobamos si es igual al estad actual
@@ -154,7 +164,7 @@ bool isFront(priority_queue<pair<Puzzle,int>,vector<pair<Puzzle,int>>,myComp > f
     return false;
 }
 
-//este metodo retorna true si el estado esta en el conjunto de explorados , en otro caso retorna false
+//este metodo retorna true si el estado esta en el conjunto de explorados
 bool fue_explorado(set<Puzzle>& nodosExplorados , Puzzle& estadoActual){
     if(nodosExplorados.find(estadoActual) != nodosExplorados.end()){
         return true; //elemento esta en el set
@@ -189,8 +199,10 @@ vector<Puzzle> generarVecinos(Puzzle& nodoActual){
     return vecinos;
 }
 
-
-bool dfs(Puzzle& estadoInicial , Puzzle& estadoFinal,int& size){
+/*lLgoritmo DFS , busqueda no informada
+  parametros(EstadoActual , meta , dimension tablero)
+*/
+bool depth_first_search(Puzzle& estadoInicial , Puzzle& estadoFinal,int& size){
     stack<Puzzle> frontera; //nodos en la frontera
     set<Puzzle> explorado; //nodos que ya han sido explorados
 
@@ -211,7 +223,7 @@ bool dfs(Puzzle& estadoInicial , Puzzle& estadoFinal,int& size){
 
         vector<Puzzle>::iterator it;
         for(it = vecinos.begin() ; it != vecinos.end() ; it++){
-            if(!enFrontera(frontera,*it) && !fue_explorado(explorado,*it)){
+            if(!fue_explorado(explorado,*it)){
                 frontera.push(*it);
                 imprimirTablero(*it,size);
             }
@@ -220,8 +232,38 @@ bool dfs(Puzzle& estadoInicial , Puzzle& estadoFinal,int& size){
     return false;
 }
 
+
+/*Algoritmo BFS , busqueda no informada*/
+bool breadth_first_search(Puzzle& estadoInicial , Puzzle& estadoFinal ,const int& size){
+    queue<Puzzle> frontera;
+    frontera.push(estadoInicial);
+    set<Puzzle> explorados;
+
+    while(!frontera.empty()){
+        Puzzle state = frontera.front();
+        frontera.pop();
+        explorados.insert(state);
+
+        if(testObjetivo(estadoFinal,state,size)){
+            imprimirTablero(estadoFinal,size);
+            return true;
+        }
+
+        vector<Puzzle> vecinos = generarVecinos(state);
+        vector<Puzzle>::iterator it;
+        for(it = vecinos.begin() ; it != vecinos.end() ; it++){
+            if(!fue_explorado(explorados,*it)){
+                frontera.push(*it);
+                explorados.insert(*it);
+                imprimirTablero(*it,size);
+            }
+        }
+    }
+    return false;
+}
+
 /*calcula el la heuristica como la distancia manhattan entre una casilla a su posicion correcta
-recibe el estadoActual y un mapa que tiene como clave el elemento actual y su valor la posicion en su
+recibe el estadoActual y un mapa que tiene como clave el elemento actual y su valor es la posicion en su
 forma correcta
 */
 int heuristic(Puzzle& estadoActual , map<int,Posicion>& goalPos, int size){
@@ -236,6 +278,7 @@ int heuristic(Puzzle& estadoActual , map<int,Posicion>& goalPos, int size){
     return h;
 }
 
+/*Genera un puzzle con un orden aleatorio */
 Puzzle generateRandomPuzzle(const int& size){
     vector<int> numeros(size*size);
     Puzzle puzzle(size,vector<int>(size));
@@ -253,7 +296,8 @@ Puzzle generateRandomPuzzle(const int& size){
     return puzzle;
 }
 
-//generar un map con el valor y la posicion en el estadp final del puzzle
+//generar un mapa con el valor y la posicion en el estadp final del puzzle
+//el cual sirve para calcular las heuristicas con respecto a las posiciones correctas
 void setPos_EstadoFinal(map<int,Posicion>& goalPos,const int& dimension){
     //recorrer el goal
     int contador = 1;
@@ -276,18 +320,20 @@ void setPos_EstadoFinal(map<int,Posicion>& goalPos,const int& dimension){
 
 
 //gredy(inicio,fin,posicionesGoal,mapHeuristicas)
-bool greedy(Puzzle& estadoActual , Puzzle& estadoFinal , map<int,Posicion>& goalPos ,map<Puzzle,int>& tableroHeuristicas){
+bool greedy(Puzzle& estadoActual , Puzzle& estadoFinal , map<int,Posicion>& goalPos ,vector<Puzzle>& camino){
     int size = estadoFinal.size();
     priority_queue<pair<Puzzle,int>,vector<pair<Puzzle,int>>,myComp > frontera;
     int h = heuristic(estadoActual,goalPos,size);
-    tableroHeuristicas[estadoActual] = h;
     frontera.push(make_pair(estadoActual,h));
     set<Puzzle> explorados;
+
 
     while(!frontera.empty()){
         Puzzle state = frontera.top().first; //tomamos el estado con la menor heuristica
         frontera.pop(); //lo sacamos de la PQ
         explorados.insert(state); //estado ya ha sido explorado
+
+        camino.push_back(state); //agrega el estado al camino
 
         if(testObjetivo(state,estadoFinal,size)){
             return true;
@@ -298,12 +344,12 @@ bool greedy(Puzzle& estadoActual , Puzzle& estadoFinal , map<int,Posicion>& goal
 
         vector<Puzzle>::iterator it;
         for(it = vecinos.begin() ; it != vecinos.end() ; it++){
-            if(!isFront(frontera,*it) && !fue_explorado(explorados,*it)){
+            /*para cada vecino del estado actual que no haya sido explorado y que no este en la enFrontera
+            calcular su heuristica y agregarlo a la frontera y marcarlo como explorado*/
+            if(!fue_explorado(explorados,*it)){
                 h = heuristic(*it,goalPos,size);
-                tableroHeuristicas[*it] = h;
                 frontera.push(make_pair(*it,h));
                 explorados.insert(*it);
-                imprimirTablero(*it,size);
             }
         }
     }
@@ -311,12 +357,129 @@ bool greedy(Puzzle& estadoActual , Puzzle& estadoFinal , map<int,Posicion>& goal
 }
 
 
+//devuelve el estado con la menor funcion f(n)
+Matriz findMin_funcion(set<Matriz> my_set){
+    Matriz min_element;
+    if (!my_set.empty()){
+        min_element = *my_set.begin();
+    }
+    return min_element;
+}
+
+
+/*ALgoritmo A y algunas funciones auxiliares*/
+bool estaEnConjunto(set<Matriz>& conjunto , Puzzle& current){
+    set<Matriz>::iterator it;
+    //recorremos el conjunto y si el test objetivo es igual al nodo que buscamos retorna true
+    for(it = conjunto.begin() ; it != conjunto.end() ; it++){
+        if(testObjetivo(it->estado,current,current.size())){
+            return true;
+        }
+    } //caso contrario retorna false
+    return false;
+}
+
+//A* NO FUNCIONA DE FORMA ADECUADA (MALA IMPLEMENTACION)
+bool A_Star(Puzzle& start , Puzzle& goal , map<int,Posicion>& goalPos){
+    int size = goal.size();
+    set<Matriz> open_list;
+    set<Matriz> closed_list;
+
+    //matriz con la info de el nodo de partida
+    Matriz infoStart;
+    infoStart.estado = start;
+    infoStart.g = 0;
+    infoStart.h = heuristic(start,goalPos,size);
+    infoStart.f = infoStart.g + infoStart.h;
+
+    //insertamos start en open_list
+    open_list.insert(infoStart);
+
+    while(!open_list.empty()){
+        Matriz current = findMin_funcion(open_list); //obtenemos el estado con menor f(n)
+
+        if(testObjetivo(current.estado , goal , size)){
+            imprimirTablero(current.estado,size);
+            return true;
+        }
+
+        open_list.erase(current); //remover de open list
+        closed_list.insert(current);  //agregar a closed list
+
+        vector<Puzzle> vecinos = generarVecinos(current.estado); //generar vecinos del estado actual
+        vector<Puzzle>::iterator it;
+        for(it = vecinos.begin() ; it != vecinos.end() ; it++){
+            Puzzle vecino = *it;
+            //convertimos un estado a su informacion total
+            Matriz info;
+            info.estado = vecino;
+            info.g = 1;
+            info.h = heuristic(vecino,goalPos,size);
+            info.f = info.h + info.g;
+
+            if(estaEnConjunto(open_list,vecino)){
+                continue;
+            }
+
+            int costo = current.g + 1;
+
+            if(estaEnConjunto(open_list,vecino) && costo < info.g){
+                open_list.erase(info);
+            }
+            if(estaEnConjunto(closed_list,vecino) && costo < info.g){
+                closed_list.erase(info);
+            }
+            if(!estaEnConjunto(open_list,vecino) && !estaEnConjunto(closed_list,vecino)){
+                open_list.insert(info);
+                info.g = costo;
+                info.h = heuristic(info.estado,goalPos,size);
+                info.f = info.g + info.h;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+void ejecutarDFS(Puzzle& start,Puzzle& goal){
+    int size = goal.size();
+    bool solucion = depth_first_search(start,goal,size);
+    if(!solucion){
+        puts("Puzzle no tiene solucion");
+    }
+}
+
+void ejecutarBFS(Puzzle& start , Puzzle& goal){
+    int size = goal.size();
+    bool solucion = breadth_first_search(start,goal,size);
+    if(!solucion){
+        puts("Puzzle no tiene solucion");
+    }
+}
+
+void ejecutarGreedy(Puzzle& start , Puzzle& goal , map<int,Posicion>& goalPos ,vector<Puzzle>& camino){
+    int size = goal.size();
+    bool solucion = greedy(start,goal,goalPos,camino);
+    if(solucion){
+        vector<Puzzle>::iterator it;
+        for(it = camino.begin() ; it != camino.end() ; it++){
+            imprimirTablero(*it,size);
+        }
+    }else{
+        puts("Puzzle no tiene solucion");
+    }
+}
+
 int main(){
 
     //definir la dimension del tablero
     int dimension;
-    puts("ingrese dimension");
+    string algoritmo;
+    puts("ingrese dimension (2,3 o 4)");
     cin >> dimension;
+    puts("probar algoritmo: dfs,bfs,greedy");
+    cin >> algoritmo;
 
     //generar el estado objetivo
     Puzzle goal = getObjetivo(dimension);
@@ -329,12 +492,16 @@ int main(){
     map<int,Posicion> goalPos;
     setPos_EstadoFinal(goalPos,dimension);
 
-    //mapa de un estado con su respectiva heuristica
-    //map <puzzle, heuristica>
-    map< Puzzle , int > heuristicasTablero;
+    vector<Puzzle> camino; //vector que guarda el camino con menor heuristica para algoritmo greedy
 
-    bool f = greedy(current,goal,goalPos,heuristicasTablero);
-    cout << f << endl;
+    //ejecucion de algoritmos
+    if(algoritmo == "dfs"){
+        ejecutarDFS(current,goal);
+    }else if(algoritmo == "bfs"){
+        ejecutarBFS(current,goal);
+    }else if(algoritmo == "greedy"){
+        ejecutarGreedy(current,goal,goalPos,camino);
+    }
 
     return 0;
 }
